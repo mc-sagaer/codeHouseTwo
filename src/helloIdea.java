f3dde2f0134da87869b12e40b2c3ab485170c7ab
ideaMc <932796837@qq.com>
2019-09-06 14:26:47 +0800

    one-

--- src/helloIdea.java
+++ src/helloIdea.java
@@ -1,6 +1,10 @@
 public class helloIdea {
 
     public static void main(String[] args) {
+        System.out.println("基于别人的项目，做一次克隆，克隆出一个一样的项目到你的账号下，这个行为叫做Fork。\n" +
+                "\n" +
+                "Fork英文是叉子的意思，表示在原项目的基础上，出现了分支");
+        System.out.println("2019-07-01");
         System.out.println("945881");
         System.out.println("2019-07-01");
     }
